59b5aa4f984e1e4a5de5532956cf0d1e1f561807
Niels Gerritsen <n.c.gerritsen@gmail.com>
2020-09-17 10:26:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Calcucli
+
+Calculator REPL running on calculy.
+
+## Install
+
+```
+npm install -g calcucli
+```
+
+## Use
+
+```
+calc
+> 1+2
+3
+> (1-2)*3
+-3
+> ans+6
+3
+> (3+3(
+Unexpected token "undefined", expected a number, parenthesis, function or constant.
+> |cos(pi)-sin(pi/2)|^2
+4
+```
+
+For all the math syntax and functions available check the documentation on calculy:
+
+[https://www.npmjs.com/package/calculy](https://www.npmjs.com/package/calculy)
+
+## User interface
+
+Also powered by [calculy](https://www.npmjs.com/package/calculy) is the beautiful and powerful Calculory web calculator.
+
+[https://calculory.netlify.app/](https://calculory.netlify.app/)
---
+++ src/index.js
@@ -0,0 +1,41 @@
+const calculy = require('calculy');
+const repl = require('repl');
+
+const RED = '\x1b[31m';
+const DEFAULT = '\x1b[0m';
+
+let ans = 0;
+
+repl.start({
+  eval: evaluate,
+});
+
+function evaluate(code, _, __, callback) {
+  try {
+    handleResult(code, callback);
+  } catch (error) {
+    handleError(error, callback);
+  }
+}
+
+function handleResult(code, callback) {
+  const result = calculy.evaluate(code, ans);
+
+  ans = result;
+
+  callback(null, result);
+}
+
+function handleError(error, callback) {
+  if (error.name !== 'SyntaxError') {
+    callback(error);
+    return;
+  }
+
+  logError(error.message.replace('SyntaxError: ', ''));
+  callback();
+}
+
+function logError(message) {
+  console.error(RED + message + DEFAULT);
+}
